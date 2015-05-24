377ff4a44d9290e360aaaf79a422350559723ff4
Arturo <arturo.romerotcs@gmail.com>
2015-05-24 03:10:44 -0600

    finished nav

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 angular
 	.module('GithubApp')
 	.controller('CollapseCtrl', function($scope) {
-		 $scope.isCollapsed = false;
+		 
 	});
--- js/controller.js
+++ js/controller.js
@@ -2,12 +2,13 @@ angular
 	.module('GithubApp')
 	.controller('GithubController', function($scope, $log, GithubService) {
 		
-		$scope.searchText = 'arturoromerotcs';
+		$scope.searchText = '';
 
 		$scope.getUserData = function(username){ 
 			GithubService.getUser(username)
 				.then(function(user) {
 					$scope.user = user;
+					$scope.isCollapsed = true;
 				});
 		}	
 });
